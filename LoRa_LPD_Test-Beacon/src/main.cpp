7016771e4e70f3854b8aeba130258e76c41fb46f
Brycycle <33557087+Brycycle@users.noreply.github.com>
2026-04-26 18:50:21 -0700

    Update main.cpp

--- LoRa_LPD_Test-Beacon/src/main.cpp
+++ LoRa_LPD_Test-Beacon/src/main.cpp
@@ -123,7 +123,13 @@ void TXandListenforACK() {
       //Serial.println("Packet transmitted, waiting for ACK...");
   }
 
+  while(radio.isTransmitting()) {
+    // Wait for transmission to complete
+    delay(10);
+  } 
+
   // Start listening for response
+  receivedFlag = false;
   switchToACKlinkChannel();
   resumeReception();
   unsigned long startTime = millis();
