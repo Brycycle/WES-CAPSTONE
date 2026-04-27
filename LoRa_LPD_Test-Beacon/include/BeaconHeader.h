7c97a99e1908c06ffd36a0b79046d9a4affa6f80
Brycycle <33557087+Brycycle@users.noreply.github.com>
2026-04-26 19:35:28 -0700

    SF12 test

--- LoRa_LPD_Test-Beacon/include/BeaconHeader.h
+++ LoRa_LPD_Test-Beacon/include/BeaconHeader.h
@@ -8,10 +8,10 @@ using namespace std;
 // LoRa channel configuration //Beacon Tx on uplink, remote unit Tx on downlink
 #define TX_FREQ 914.9
 #define TX_BW 125.0       // 10.4, 125, 500
-#define TX_SF 7           // 7, 12
+#define TX_SF 12           // 7, 12
 #define ACK_FREQ 923.3
 #define ACK_BW 125.0       // 10.4, 125, 500
-#define ACK_SF 7           // 7, 12
+#define ACK_SF 12           // 7, 12
 
 
 #define RESPONSE_LISTEN_WINDOW 250
